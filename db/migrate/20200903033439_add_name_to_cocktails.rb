fc2c77dd00c35763d6a3dc38c29ed95e9d4786c5
Maite Eguren Brown <maite.eguren.brown@gmail.com>
2020-09-03 13:42:35 +1000

    4 failures left

---
+++ db/migrate/20200903033439_add_name_to_cocktails.rb
@@ -0,0 +1,4 @@
+class AddNameToCocktails < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_030239) do
+ActiveRecord::Schema.define(version: 2020_09_03_033439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2020_09_03_030239) do
   end
 
   create_table "doses", force: :cascade do |t|
-    t.string "name"
     t.text "description"
     t.bigint "cocktail_id", null: false
     t.bigint "ingredient_id", null: false
@@ -33,9 +32,9 @@ ActiveRecord::Schema.define(version: 2020_09_03_030239) do
   end
 
   create_table "ingredients", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   add_foreign_key "doses", "cocktails"
