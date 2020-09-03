2729d95c109d638f038d7867a5beb08a33e64bd8
Maite Eguren Brown <maite.eguren.brown@gmail.com>
2020-09-03 15:15:20 +1000

    cocktails actions & views done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,33 @@
 class CocktailsController < ApplicationController
+    def index
+        @cocktails = Cocktail.all
+    end
+
+    def new
+        @cocktail = Cocktail.new
+    end
+    
+    def create
+        @cocktail = Cocktail.new(cocktail_params)
+        @cocktail.save!
+
+        redirect_to cocktails_path
+    end
+
+    # def show
+    # end
+
+    # def edit
+    # end
+
+    # def update
+    # end
+
+    # def destroy
+    # end
+    private
+
+    def cocktail_params
+        params.require(:cocktail).permit(:name)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,18 +12,30 @@ require 'open-uri'
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open(url).read
 ingredients = JSON.parse(ingredients_serialized)
-randomIngredient = ingredients["drinks"][rand(0..100)]["strIngredient1"]
 
 puts "Cleaning database"
 
 Ingredient.destroy_all
 
-
 puts "Creating ingredients..."
 
-10.times do
-    ingredient = Ingredient.new(randomIngredient)
-    ingredient.save!
+while (Ingredient.count < 10) do
+    randomIngredient = ingredients["drinks"][rand(100)]["strIngredient1"]
+    Ingredient.create(name: randomIngredient)
 end
 
 puts "Created #{Ingredient.count} ingredient(s)"
+
+puts "Cleaning database"
+
+Cocktail.destroy_all
+
+
+puts "Creating cocktails..."
+
+while (Cocktail.count < 10) do
+    randomIngredient = ingredients["drinks"][rand(100)]["strIngredient1"]
+    Cocktail.create(name: randomIngredient)
+end
+
+puts "Created #{Cocktail.count} ingredient(s)"
