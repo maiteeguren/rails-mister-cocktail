040287fa364253863cc226b7e4ffa9f3d730efe9
Maite Eguren Brown <maite.eguren.brown@gmail.com>
2020-09-11 19:05:35 +1000

    updating

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,22 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    const banner = document.querySelector(".banner");
+    const logo = document.querySelector("#logo");
+
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        //banner.style.height = "50vh";
+        logo.style.height = "120px";
+        banner.style.position("sticky");
+    } else {
+        banner.style.height = "100vh";
+        logo.style.height = "248px"; 
+        banner.classList.remove("fixed");
+    }
+}
+
