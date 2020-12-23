eec4c5f67d3eb1364ae7bb55ec354dcdaa349f56
maiteeguren <maite.eguren.brown@gmail.com>
2020-12-23 19:35:23 +1100

    instructions added to cocktail table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_031640) do
+ActiveRecord::Schema.define(version: 2020_12_23_082651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_10_09_031640) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "instructions"
   end
 
   create_table "doses", force: :cascade do |t|
