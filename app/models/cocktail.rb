b909b00a6e260a93d889e392483153d8ba020ff1
maiteeguren <maite.eguren.brown@gmail.com>
2020-12-31 10:09:59 +1100

    restrictions added to models

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,8 +3,9 @@ class Cocktail < ApplicationRecord
     #has_many :ingredients, through: :doses, dependent: :destroy
     has_many :measures, through: :doses, dependent: :destroy
     has_one_attached :photo
-    has_many :reviews
+    has_many :reviews, dependent: :destroy
     belongs_to :user
 
     validates :name, uniqueness: true, presence: true
+    validates :user, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :cocktail
+  belongs_to :user
 
   validates :user, uniqueness: { scope: :cocktail }, presence: true
   validates :cocktail, presence: true
-  validates :description, presence: true
+  validates :content, presence: true
   validates :rating, presence:true, inclusion: { in: (1..5) }
 end
