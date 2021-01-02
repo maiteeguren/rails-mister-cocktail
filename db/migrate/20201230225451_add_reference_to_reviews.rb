ebe34a96af8d717075ff8299b03b64e84d7d7d87
maiteeguren <maite.eguren.brown@gmail.com>
2021-01-02 14:17:06 +1100

    pg bug not null error solved

--- db/migrate/20201230225451_add_reference_to_reviews.rb
+++ db/migrate/20201230225451_add_reference_to_reviews.rb
@@ -1,6 +1,6 @@
 class AddReferenceToReviews < ActiveRecord::Migration[6.0]
   def change
     remove_column :reviews, :user, :string
-    add_reference :reviews, :user, null: false, foreign_key: true
+    add_reference :reviews, :user, foreign_key: true
   end
 end
